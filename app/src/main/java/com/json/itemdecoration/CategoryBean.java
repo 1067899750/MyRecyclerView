e6a5384dab03346a4021f268401f25e1af547e51
yantao.pu <12345678>
2018-11-22 17:13:43 +0800

    first commit

---
+++ app/src/main/java/com/json/itemdecoration/CategoryBean.java
@@ -0,0 +1,40 @@
+package com.json.itemdecoration;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Description  实体类
+ * Author puyantao
+ * Email 1067899750@qq.com
+ * Date 2018-11-22 17:07
+ */
+
+
+public class CategoryBean implements Serializable{
+    private static final long serialVersionUID = 5136218664701666396L;
+
+    private String tag;//所属的分类
+    private String categoryName;
+
+    public CategoryBean(String tag, String categoryName) {
+        this.tag = tag;
+        this.categoryName = categoryName;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
---
+++ app/src/main/java/com/json/itemdecoration/MainActivity.java
@@ -0,0 +1,55 @@
+package com.json.itemdecoration;
+
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TitleItemDecoration titleItemDecoration;
+    private RecyclerView recyclerView;
+    private CategoryAdapter adapter;
+    private List<CategoryBean> lists;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //设置测试数据
+        setDatas();
+
+        adapter = new CategoryAdapter(this, lists);
+        recyclerView.setAdapter(adapter);
+        titleItemDecoration = new TitleItemDecoration(this, lists);
+        recyclerView.addItemDecoration(titleItemDecoration);
+
+    }
+
+    private void setDatas() {
+        lists = new ArrayList<>();
+        lists.add(new CategoryBean("A", "哎"));
+        lists.add(new CategoryBean("A", "爱"));
+        lists.add(new CategoryBean("A", "昂"));
+        lists.add(new CategoryBean("B", "beautiful"));
+        lists.add(new CategoryBean("B", "beak"));
+        lists.add(new CategoryBean("B", "but"));
+        lists.add(new CategoryBean("B", "bring"));
+        lists.add(new CategoryBean("C", "category"));
+        lists.add(new CategoryBean("C", "can"));
+        lists.add(new CategoryBean("C", "captive"));
+        lists.add(new CategoryBean("C", "computer"));
+        lists.add(new CategoryBean("D", "default"));
+
+
+    }
+
+}
