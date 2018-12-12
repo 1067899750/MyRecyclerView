14d92554925f27d740525fcea8a0553cfdf6fd86
yantao.pu <12345678>
2018-12-12 09:31:07 +0800

    recyclerView联动列表

--- app/src/main/java/com/json/itemdecoration/MainActivity.java
+++ app/src/main/java/com/json/itemdecoration/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, TwoRecyActivity.class));
+//        startActivity(new Intent(MainActivity.this, TwoRecyActivity.class));
         initView();
     }
 
