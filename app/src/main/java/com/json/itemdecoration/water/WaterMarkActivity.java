866029223c29d5911893e7a450f7f8885e742e55
yantao.pu <12345678>
2018-12-14 16:05:54 +0800

    水印

--- app/src/main/java/com/json/itemdecoration/water/WaterMarkActivity.java
+++ app/src/main/java/com/json/itemdecoration/water/WaterMarkActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.json.itemdecoration.R;
@@ -22,7 +23,7 @@ import butterknife.ButterKnife;
 public class WaterMarkActivity extends AppCompatActivity {
 
     @BindView(R.id.rlLogo)
-    RelativeLayout rlLogo;
+    View rlLogo;
 
     private Context mContext;
 
