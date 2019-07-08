928a03d6a3300e575b6239a077da26cee52a473c
puyantao <puyantao123759>
2019-07-08 09:26:53 +0800

    轮廓图

--- app/src/main/java/com/json/itemdecoration/MainActivity.java
+++ app/src/main/java/com/json/itemdecoration/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        TextActivity.startTextActivity(MainActivity.this);
+//        TextActivity.startTextActivity(MainActivity.this);
 
     }
 
